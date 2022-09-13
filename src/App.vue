18f0863d36af9a21689148c09994f6d4b52583df
Margarita Sergeeva <margaritk@inbox.ru>
2022-09-13 15:07:46 +0300

    added project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,51 @@
 <template>
   <div class="container">
-    <div class="card">
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+    <app-people-list
+      :people="people"
+      @load = "loadPeople"
+    ></app-people-list>
   </div>
 </template>
 
 <script>
+import AppPeopleList from '@/AppPeopleList'
+
 export default {
+  data () {
+    return {
+      name: '',
+      people: []
+    }
+  },
+  methods: {
+    async createPerson () {
+      const response = await fetch('https://pet-vue-http-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const personData = await response.json()
+      console.log(personData)
+
+      this.name = ''
+    }
+  },
+  components: {
+    AppPeopleList
+  }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/AppPeopleList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="people.length">
+    <div v-for="person in people" :key="person" class="card">
+      <h3>{{ person }}</h3>
+    </div>
+  </div>
+  <div v-else class="card">
+    <h4>Данных пока нет</h4>
+    <button class="btn" @click="$emit('load')">Загрузить список людей</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['load'],
+  props: ['people']
+}
+</script>
